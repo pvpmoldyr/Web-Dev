1ded94654965d7eccddc0ebe6d0ea45c4ec9ea8d
pvpmoldyr <mo_sharipova@kbtu.kz>
2026-02-17 01:33:15 +0600

    Обновление lab4

--- Web-Dev/Lab4/task1/learn-angular/src/app/app.ts
+++ Web-Dev/Lab4/task1/learn-angular/src/app/app.ts
@@ -1,3 +1,18 @@
+/*
+////////////////////////////////////////////////////////1
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: ` Hello {{ city }}, {{ 1 + 1 }} `,
+})
+export class App {
+  city = 'San Francisco';
+}
+*/
+
+/*
+///////////////////////////////////////////////////////2
 import {Component} from '@angular/core';
 
 @Component({
@@ -14,3 +29,270 @@ export class User {
   imports: [],
 })
 export class App {}
+*/
+
+/*
+////////////////////////////////////////////////////3
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  template: ` Username: {{ username }} `,
+})
+export class User {
+  username = 'youngTech';
+}
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <section>
+      <app-user />
+    </section>
+  `,
+  imports: [User],
+})
+export class App {}
+
+//////////////////////////////////////////////////////4
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    @if (isServerRunning) {
+      <span>Yes, the server is running</span>
+    } @else {
+      <span>No, the server is not running</span>
+    }
+  `,
+})
+export class App {
+  isServerRunning = true;
+}
+
+///////////////////////////////////////////////5
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    @for (user of users; track user.id) {
+      <p>{{ user.name }}</p>
+    }
+  `,
+})
+export class App {
+  users = [
+    {id: 0, name: 'Sarah'},
+    {id: 1, name: 'Amy'},
+    {id: 2, name: 'Rachel'},
+    {id: 3, name: 'Jessica'},
+    {id: 4, name: 'Poornima'},
+  ];
+}
+
+
+////////////////////////////////////////////////////////6
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  styleUrls: ['app.css'],
+  template: ` <div [contentEditable]="isEditable"></div> `,
+})
+export class App {
+  isEditable = true;
+}
+
+//////////////////////////////////////////////////////////////7
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <section (mouseover)="showSecretMessage()">
+      There's a secret message for you, hover to reveal 👀
+      {{ message }}
+    </section>
+  `,
+})
+export class App {
+  message = '';
+
+  showSecretMessage() {
+  this.message = 'Way to go ';
+}
+}
+
+//////////////////////////////////////////////////////////////////8
+user.ts
+import {Component, input} from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  template: ` <p>The user's name is {{ name() }}</p> `,
+})
+export class User {
+  readonly name = input<string>();
+}
+app.ts
+import {Component} from '@angular/core';
+import {User} from './user';
+
+@Component({
+  selector: 'app-root',
+  template: ` <app-user name="Simran" /> `,
+  imports: [User],
+})
+export class App {}
+
+/////////////////////////////////////////////////////9
+childd.ts
+import {Component, output} from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  styles: `
+    .btn {
+      padding: 5px;
+    }
+  `,
+  template: ` <button class="btn" (click)="addItem()">Add Item</button> `,
+})
+export class Child {
+  readonly addItemEvent = output<string>();
+
+  addItem() {
+    this.addItemEvent.emit('🐢');
+  }
+}
+app.ts
+import {Component} from '@angular/core';
+import {Child} from './child';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <app-child (addItemEvent)="addItem($event)" />
+    <p>🐢 all the way down {{ items.length }}</p>
+  `,
+  imports: [Child],
+})
+export class App {
+  items = new Array();
+
+  addItem(item: string) {
+    this.items.push(item);
+  }
+}
+
+////////////////////////////////////10 Deferrable Views
+
+import {Component} from '@angular/core';
+import {Comments} from './comments';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <div>
+      <h1>How I feel about Angular</h1>
+      <article>
+  <p>
+    Angular is my favorite framework, and this is why. Angular has the coolest deferrable view
+    feature that makes defer loading content the easiest and most ergonomic it could possibly be.
+    The Angular community is also filled with amazing contributors and experts that create excellent
+    content. The community is welcoming and friendly, and it really is the best community out there.
+  </p>
+  <p>
+    I can't express enough how much I enjoy working with Angular. It offers the best developer
+    experience I've ever had. I love that the Angular team puts their developers first and takes
+    care to make us very happy. They genuinely want Angular to be the best framework it can be, and
+    they're doing such an amazing job at it, too. This statement comes from my heart and is not at
+    all copied and pasted. In fact, I think I'll say these exact same things again a few times.
+  </p>
+  <p>
+    Angular is my favorite framework, and this is why. Angular has the coolest deferrable view
+    feature that makes defer loading content the easiest and most ergonomic it could possibly be.
+    The Angular community is also filled with amazing contributors and experts that create excellent
+    content. The community is welcoming and friendly, and it really is the best community out there.
+  </p>
+  <p>
+    I can't express enough how much I enjoy working with Angular. It offers the best developer
+    experience I've ever had. I love that the Angular team puts their developers first and takes
+    care to make us very happy. They genuinely want Angular to be the best framework it can be, and
+    they're doing such an amazing job at it, too. This statement comes from my heart and is not at
+    all copied and pasted. In fact, I think I'll say these exact same things again a few times.
+  </p>
+  <p>
+    Angular is my favorite framework, and this is why. Angular has the coolest deferrable view
+    feature that makes defer loading content the easiest and most ergonomic it could possibly be.
+    The Angular community is also filled with amazing contributors and experts that create excellent
+    content. The community is welcoming and friendly, and it really is the best community out there.
+  </p>
+  <p>
+    I can't express enough how much I enjoy working with Angular. It offers the best developer
+    experience I've ever had. I love that the Angular team puts their developers first and takes
+    care to make us very happy. They genuinely want Angular to be the best framework it can be, and
+    they're doing such an amazing job at it, too. This statement comes from my heart and is not at
+    all copied and pasted.
+  </p>
+</article>
+      @defer(on viewport){
+      <comments /> 
+      } @placeholder {
+  <p>Future comments</p>
+}@loading(minimum 2s){
+  <p>Loading comments...</p>
+}
+      
+    </div>
+  `,
+  imports: [Comments],
+})
+export class App {}
+
+comments.ts
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'comments',
+  template: `
+    <ul>
+      <li>Building for the web is fantastic!</li>
+      <li>The new template syntax is great</li>
+      <li>I agree with the other comments!</li>
+    </ul>
+  `,
+})
+export class Comments {}
+
+/////////////////////////////////////11 Optimizing images
+import {Component} from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-user',
+  template: `
+    <p>Username: {{ username }}</p>
+    <p>Preferred Framework:</p>
+    <ul>
+      <li>
+        Static Image:
+        <img ngSrc="/logo.svg" alt="Angular logo" width="32" height="32" />
+      </li>
+      <li>
+        Dynamic Image:
+        <img [ngSrc]="logoUrl" [alt]="logoAlt" width="32" height="32" />
+      </li>
+    </ul>
+  `,
+  imports: [NgOptimizedImage],
+})
+export class User {
+  logoUrl = '/logo.svg';
+  logoAlt = 'Angular logo';
+  username = 'youngTech';
+}
+
+*/ 
---
+++ Web-Dev/Lab4/task1/learn-angular/taskks/21Pipes.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+import {LowerCasePipe} from '@angular/common';
+@Component({
+  selector: 'app-root',
+  template: `{{ username | lowercase }}`,
+    imports: [LowerCasePipe],
+})
+export class App {
+  username = 'yOunGTECh';
+}
