daa53093b5e94d0e9503cd0f9593206f5d998b95
pvpmoldyr <mo_sharipova@kbtu.kz>
2026-04-01 01:50:50 +0600

    dlastd

--- Lab9/api/admin.py
+++ Lab9/api/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Category, Product
 
-admin.site.register(Category)
-admin.site.register(Product)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id' , 'name')
+    search_fields = ('name',)
+
+@admin.register(Product)
+class ProductAdmon(admin.ModelAdmin):
+    list_display = ('id' , 'name' , 'price' , 'description' , 'category')
+    search_fields = ('name',)
--- Lab9/api/models.py
+++ Lab9/api/models.py
@@ -3,6 +3,16 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=255)
 
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+    
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
     def __str__(self):
         return self.name
 
