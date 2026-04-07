22e01e16858c0d9b24962f44650eecb6b4c15959
pvpmoldyr <mo_sharipova@kbtu.kz>
2026-04-07 15:15:16 +0600

    Lab10

--- Lab10/api/views/__init__.py
+++ Lab10/api/views/__init__.py
@@ -1,1 +1,13 @@
+# api/views/__init__.py
+
+# Level 2: Function-Based Views
+# from .fbv import *
+
+# Level 3: Class-Based Views
+# from .cbv import *
+
+# Level 4: Mixins
+# from .mixins import *
+
+# Level 5: Generic Views 
 from .generics import *
--- Lab10/api/views/mixins.py
+++ Lab10/api/views/mixins.py
@@ -1,8 +1,10 @@
 from rest_framework import generics, mixins
-from api.models import Product
-from api.serializers import ProductSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from api.models import Product, Category  
+from api.serializers import ProductSerializer, CategorySerializer  
 
-class ProductListAPIView(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+class ProductListAPIView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
@@ -12,10 +14,11 @@ class ProductListAPIView(mixins.ListModelMixin,mixins.CreateModelMixin,generics.
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
 
+
 class ProductDetailAPIView(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    lookup_url_kwarg = 'product_id' 
+    lookup_url_kwarg = 'product_id'
 
     def get(self, request, *args, **kwargs):
         return self.retrieve(request, *args, **kwargs)
@@ -25,3 +28,34 @@ class ProductDetailAPIView(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, m
 
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
+
+
+class CategoryListAPIView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class CategoryDetailAPIView(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+class CategoryProductsAPIView(APIView):
+    def get(self, request, category_id):
+        products = Product.objects.filter(category_id=category_id)
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
